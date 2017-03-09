f7b352b954e240352ba63b6e6398bc07e660f65e
Cedric <cedric.vigne11@gmail.com>
2017-03-09 03:20:31 +0100

    Correctionoubli conflit

--- gen.ml
+++ gen.ml
@@ -21,7 +21,6 @@ let position = fun elm list -> positionAux(elm, 0, list);;
 
 let rec gen_exp = fun vList exp -> match exp with
 (Const(IntT, IntV(v))) -> [Loadc(IntT, IntV(v))]
-<<<<<<< HEAD
 |(Const(BoolT, BoolV(v))) -> if(v = true) 
 			     then [Loadc(BoolT, IntV (1))] 
 			     else [Loadc(BoolT, IntV(0))]
